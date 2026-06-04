19ea702e58c3c029e1a81630f00435f3e6cbd637
Ibrar <94802564+OrangeHubX@users.noreply.github.com>
2026-06-04 23:26:36 +0500

    Added install as pwa option

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'material-timer-v1';
+const CACHE_NAME = 'material-timer-v1.5';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './timer_tone.mp3',
+  './timer-alarm.mp3',
   'https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,1,0',
   'https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500&family=Roboto+Mono:wght@300;400&display=swap'
 ];
